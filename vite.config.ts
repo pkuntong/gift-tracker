483d1341f719a28fe87bfa14389f6179efb2374d
pkuntong <your.email@example.com>
2025-04-30 21:35:39 -0400

    updating vite

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,15 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   server: {
     port: 3000,
     open: true,
