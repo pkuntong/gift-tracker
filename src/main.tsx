a6dbba2345252e4880a6f82b48d525c9e7b84225
pkuntong <your.email@example.com>
2025-05-14 13:00:46 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Import CSS using import statement (Vite will handle this properly)
 import './index.css';
 import { validateEnv } from './utils/env';
 
